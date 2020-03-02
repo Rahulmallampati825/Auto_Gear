7e7b97cb153b2bcab11501cc36395300e6530ba4
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-03-01 18:30:43 -0600

    Performing action for buttons in login

--- app/src/main/java/com/example/autogear/LoginActivity.java
+++ app/src/main/java/com/example/autogear/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.autogear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+     Button loginBTN;
+     TextView link_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBTN = findViewById(R.id.userLoginBTN);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+
+        link_signup = findViewById(R.id.link_signup);
+        link_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+
+            }
+        });
     }
 }
