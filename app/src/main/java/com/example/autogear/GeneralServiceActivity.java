cb0887128fa88cabb85e70c615e0ae72d9e4ee9e
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-03-01 18:15:38 -0600

    General Service Activity is created

---
+++ app/src/main/java/com/example/autogear/GeneralServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.autogear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneralServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_service);
+    }
+}
