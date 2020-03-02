869c83cefc3708c67b22fcf381951240909bc716
BharathKanagala <bharathkanagala19@gmail.com>
2020-03-01 22:42:47 -0600

    Adding data picker to appointment activity and added intents

--- app/src/main/java/com/example/autogear/GeneralServiceActivity.java
+++ app/src/main/java/com/example/autogear/GeneralServiceActivity.java
@@ -1,14 +1,28 @@
 package com.example.autogear;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class GeneralServiceActivity extends AppCompatActivity {
+
+
+    Button sendBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_service);
+
+        sendBTN = findViewById(R.id.sendBTN);
+        sendBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GeneralServiceActivity.this, AppointmentActivity.class));
+            }
+        });
     }
 }
