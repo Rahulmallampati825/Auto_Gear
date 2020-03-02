167cb7d346d59a360134fb9d2ebe46d5f1ba2375
BharathKanagala <bharathkanagala19@gmail.com>
2020-03-01 22:09:04 -0600

    Adding towing activity

---
+++ app/src/main/java/com/example/autogear/TowingActivity.java
@@ -0,0 +1,14 @@
+package com.example.autogear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TowingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_towing);
+    }
+}
