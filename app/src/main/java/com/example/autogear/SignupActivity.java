9d4b89833e254c1ea5bb970915138233eeebbd83
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-03-01 18:33:31 -0600

    Performing action for buttons in signup

--- app/src/main/java/com/example/autogear/SignupActivity.java
+++ app/src/main/java/com/example/autogear/SignupActivity.java
@@ -2,7 +2,13 @@ package com.example.autogear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button registerBTN = findViewById(R.id.RegisterBTN);
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            }
+        });
+
+        TextView link_signin = findViewById(R.id.link_signin);
+        link_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+
+            }
+        });
     }
 }
