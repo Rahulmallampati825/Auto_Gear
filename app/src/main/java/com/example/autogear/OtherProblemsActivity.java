ca777b27392356854d4157bfa46726abdc97205e
manasaginjupalli407@gmail.com <IluvVamsi123!>
2020-03-02 21:01:59 -0600

    Adding navigation to other problem activity

--- app/src/main/java/com/example/autogear/OtherProblemsActivity.java
+++ app/src/main/java/com/example/autogear/OtherProblemsActivity.java
@@ -2,13 +2,33 @@ package com.example.autogear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OtherProblemsActivity extends AppCompatActivity {
+    Button sendBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_problems);
+
+
+        sendBTN = findViewById(R.id.sendBTN);
+        sendBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OtherProblemsActivity.this, Otherproblem_appointments.class));
+            }
+        });
+        }
     }
-}
+
+
+
+
+
+
--- app/src/main/java/com/example/autogear/Otherproblem_appointments.java
+++ app/src/main/java/com/example/autogear/Otherproblem_appointments.java
@@ -1,4 +1,58 @@
 package com.example.autogear;
 
-public class Otherproblem_appointments {
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class Otherproblem_appointments extends AppCompatActivity implements
+        View.OnClickListener {
+
+    Button datePickerBTN;
+    TextView txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+        datePickerBTN = findViewById(R.id.datePickerBTN);
+        datePickerBTN.setOnClickListener(this);
+        txtDate =findViewById(R.id.dateTV);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == datePickerBTN) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+    }
 }
+
