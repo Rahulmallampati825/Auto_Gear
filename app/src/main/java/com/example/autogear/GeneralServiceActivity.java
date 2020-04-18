42b14aed2e3350db198d0d669c5850b742b7d379
BharathKanagala <bharathkanagala19@gmail.com>
2020-04-17 21:10:36 -0500

    Storing data to fire base

--- app/src/main/java/com/example/autogear/GeneralServiceActivity.java
+++ app/src/main/java/com/example/autogear/GeneralServiceActivity.java
@@ -2,27 +2,79 @@ package com.example.autogear;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GeneralServiceActivity extends AppCompatActivity {
 
 
     Button sendBTN;
+    private EditText yearET;
+    private EditText modelET;
+    private EditText milesET;
+    FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_service);
 
+
+
         sendBTN = findViewById(R.id.sendBTN);
         sendBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                String year = yearET.getText().toString().trim();
+                String model = modelET.getText().toString().trim();
+                String miles = milesET.getText().toString().trim();
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Year", year);
+                user.put("Model", model);
+                user.put("Miles", miles);
+
+// Add a new document with a generated ID
+                db.collection("General service")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(GeneralServiceActivity.this, "Data added ", Toast.LENGTH_SHORT).show();
+                                //Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                               // Log.w(TAG, "Error adding document", e);
+                                Toast.makeText(GeneralServiceActivity.this,"Error occured",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                finish();
                 startActivity(new Intent(GeneralServiceActivity.this, AppointmentActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/autogear/OtherProblemsActivity.java
+++ app/src/main/java/com/example/autogear/OtherProblemsActivity.java
@@ -1,14 +1,27 @@
 package com.example.autogear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OtherProblemsActivity extends AppCompatActivity {
     Button sendBTN;
+    EditText problemET,othermodelET,otherYearET,otherMilesET;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +29,51 @@ public class OtherProblemsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_problems);
 
+        db = FirebaseFirestore.getInstance();
+        problemET = (EditText) findViewById(R.id.problemET);
+        othermodelET = (EditText) findViewById(R.id.othermodelET);
+        otherYearET = (EditText) findViewById(R.id.otherYearET);
+        otherMilesET = (EditText) findViewById(R.id.otherMilesET);
 
-        sendBTN = findViewById(R.id.sendBTN);
+
+
+        sendBTN = findViewById(R.id.otherSendBTN);
         sendBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String problem = problemET.getText().toString();
+                String model = othermodelET.getText().toString();
+                String year = otherYearET.getText().toString();
+                String miles = otherMilesET.getText().toString();
+
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Year", year);
+                user.put("Model", model);
+                user.put("Miles", miles);
+                user.put("Problem",problem);
+
+// Add a new document with a generated ID
+                db.collection("Other Problems")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(OtherProblemsActivity.this, "Data added ", Toast.LENGTH_SHORT).show();
+                                //Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Log.w(TAG, "Error adding document", e);
+                                Toast.makeText(OtherProblemsActivity.this,"Error occured",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                finish();
                 startActivity(new Intent(OtherProblemsActivity.this, Otherproblem_appointments.class));
             }
         });
