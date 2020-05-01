936f0dae4f73155d58038ca6bc8a7c1109e178e6
Rahulmallampati825 <rahulmallampati825@gmail.com>
2020-04-30 21:38:37 -0500

    Added the MapActivity java file

---
+++ app/src/main/java/com/example/autogear/MapsActivity.java
@@ -0,0 +1,90 @@
+package com.example.autogear;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Address myAddress ;
+    Intent getMapIntent;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        myAddress = null;
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        getMapIntent = getIntent();
+        String location = getMapIntent.getStringExtra("start");
+        List<Address> addressList = null;
+        MarkerOptions markerOptions = new MarkerOptions();
+        Log.d("location = ", location);
+
+        if (!location.equals("")) {
+            Geocoder geocoder = new Geocoder(getApplicationContext());
+            try {
+                addressList = geocoder.getFromLocationName(location, 5);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (addressList != null) {
+                for (int i = 0; i < addressList.size(); i++) {
+                    myAddress = addressList.get(i);
+                    LatLng latLng = new LatLng(myAddress.getLatitude(), myAddress.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                }
+            }
+
+        }
+
+//
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    }
+}
